d03bd0fefffab532938e594a127f8fc20549ea0f
Kara Jensen <mail@karajensen.com>
2015-07-18 15:06:12 +0800

    adding base framework for cross platform development unit

---
+++ AIEAdvancedDiploma/CrossPlatformDevelopment/CrossPlatformDevelopment/Application.cs
@@ -0,0 +1,101 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////
+// Kara Jensen - mail@karajensen.com - Application.cs
+////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CrossPlatformDevelopment
+{
+    /// <summary>
+    /// The main application instance
+    /// </summary>
+    public class Application : Game
+    {
+        GraphicsDeviceManager m_graphics;
+        SpriteBatch m_spriteBatch;
+
+        SpriteFont m_calibri_14;
+        SpriteFont m_calibri_18;
+        SpriteFont m_calibri_22;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Application() 
+            : base()
+        {
+            m_graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Game logic initialistaion
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Load game graphics/content
+        /// </summary>
+        protected override void LoadContent()
+        {
+            m_spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            m_calibri_14 = Content.Load<SpriteFont>("Calibri_14");
+            m_calibri_18 = Content.Load<SpriteFont>("Calibri_18");
+            m_calibri_22 = Content.Load<SpriteFont>("Calibri_22");
+        }
+
+        /// <summary>
+        /// Unload game graphics/content
+        /// </summary>
+        protected override void UnloadContent()
+        {
+        }
+
+        /// <summary>
+        /// Ticks the game
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || 
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// Renders the game
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            m_spriteBatch.Begin();
+
+            float yPos = 10;
+            float xPos = 10;
+            m_spriteBatch.DrawString(m_calibri_14, "Size 14: Calibri", new Vector2(xPos, yPos), Color.White);
+            yPos += m_calibri_14.LineSpacing;
+            m_spriteBatch.DrawString(m_calibri_18, "Size 18: Calibri", new Vector2(xPos, yPos), Color.White);
+            yPos += m_calibri_18.LineSpacing;
+            m_spriteBatch.DrawString(m_calibri_22, "Size 22: Calibri", new Vector2(xPos, yPos), Color.White);
+
+            m_spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ AIEAdvancedDiploma/CrossPlatformDevelopment/CrossPlatformDevelopment/readme.txt
@@ -0,0 +1,13 @@
+
+=================================================================
+AIE Advanced Diploma - Cross Platform Development Unit
+=================================================================
+mail@KaraJensen.com
+www.KaraJensen.com
+https://github.com/karajensen/course-projects.git
+=================================================================
+
+RELEASE REQUIREMENTS: Windows 7+
+BUILD REQUIREMENTS: Windows 7+, Visual Studio 2013 (v120)
+
+=================================================================
